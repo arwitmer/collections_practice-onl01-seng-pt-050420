a85b22a60b9869a1a4279102f4e287f27bce9675
imperative-tuple-4883 <andrew.witmer445@gmail.com>
2020-05-16 22:59:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
+def sort_array_asc(array)
+  integers = [25, 7, 1] 
+  integers.sort 
+end
 
+def sort_array_desc(array)
+  integers = [25, 7, 14]
+  integers.sort! {|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  strings = ["dogs", "cat", "Horses"]
+  strings.sort {|left, right| left.length <=> right.length}
+end
+
+def swap_elements(array)
+  names = ["blake", "ashley", "scott"]
+  names.sort 
